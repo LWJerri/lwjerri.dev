55c41e1996ea4889956e8f4e07e95d07d5903330
LWJerri <loginmanx@gmail.com>
2023-01-12 04:00:39 +0200

    Finish `Main` page

--- src/pages/Main.svelte
+++ src/pages/Main.svelte
@@ -2,6 +2,7 @@
   import { onMount } from "svelte";
   import Footer from "../components/Footer.svelte";
   import Navbar from "../components/Navbar.svelte";
+  import emojisList from "../helpers/emojis.json";
 
   const oneYearToMs = 1000 * 60 * 60 * 24 * 365;
   const calculateTime = new Date().getTime() - new Date("2005-04-28").getTime();
@@ -11,25 +12,18 @@
   let emoji = "";
 
   onMount(async () => {
-    const emojiArrayRequest = await fetch(
-      "https://gist.githubusercontent.com/LWJerri/6a7bf88b133b4e7b33b709ab89e9f507/raw/5213898326bded6ca20367fd73b3776522c8c3e9/gboard-emojis.json",
-    );
-    const emojiResponse: Array<string> = await emojiArrayRequest.json();
-
-    const emojiList = emojiResponse.filter((item) => item.length <= 5);
-
-    emoji = screenSize >= 600 ? emojiList[Math.floor(Math.random() * emojiList.length) + 1] : emoji;
+    emoji = screenSize >= 600 ? emojisList[Math.floor(Math.random() * emojisList.length)] : emoji;
   });
 </script>
 
-<div class="flex flex-col h-screen justify-between bg-[#0C0E10]" style="font-family: Monocraft;">
+<div class="flex flex-col h-screen justify-between bg-[#0C0E10] scroll-smooth" style="font-family: Monocraft;">
   <Navbar />
 
   <div class="hero flex items-center justify-center h-screen">
     <div class="hero-content text-center">
       <div class="text-white">
         <h1 class="text-5xl font-bold outline-none">
-          Hey {emoji} I'm Andrey,<br />
+          Hey {emoji ?? "^-^"} I'm Andrey,<br />
           <span style="color: #5865F2;">a developer</span>
         </h1>
 
@@ -38,7 +32,7 @@
         </p>
 
         <div class="mt-10">
-          <a class=" hover:text-[#ED4245] text-white text-2xl duration-500" href="/projects">[Projects]</a>
+          <a class="hover:text-[#ED4245] text-white text-2xl duration-500" href="/projects">[Projects]</a>
           <a class="hover:text-[#ED4245] text-white text-2xl duration-500" href="/about">[About]</a>
         </div>
       </div>
