bfe1d0771996c91247fb005cf5bfb573ec31b0ea
LWJerri <loginmanx@gmail.com>
2023-01-12 03:59:45 +0200

    Made more pretty `Navbar` component

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,27 +1,45 @@
 <script lang="ts">
-  const links = [
+  import { onMount } from "svelte";
+
+  interface Links {
+    name: string;
+    url: string;
+  }
+
+  let links = [
     { name: "My projects", url: "/projects" },
     { name: "About me", url: "/about" },
-    { name: "Support me", url: "https://send.monobank.ua/8webyivBtV", isNewTab: true },
-  ] as Array<{ name: string; url: string; isNewTab?: boolean }>;
+  ] as Array<Links>;
 
-  const isMainLink = window.location.pathname === "/";
+  onMount(async () => {
+    const gistRequest = await fetch("https://api.lwjerri.dev/data");
+    const { navBarLinks } = await gistRequest.json();
+
+    links = [...links] as Array<Links>;
+
+    navBarLinks.map(({ name, url }) => {
+      return links.push({ name, url });
+    });
+  });
+
+  const isMainPage = window.location.pathname === "/";
 </script>
 
-<div class="navbar flex {isMainLink ? 'justify-end' : 'justify-between'}">
-  {#if !isMainLink}
-    <a class="hover:text-[#ED4245] duration-500 text-2xl text-white" href="/">[Home]</a>
+<div class="navbar flex {isMainPage ? 'justify-end' : 'justify-between'}">
+  {#if !isMainPage}
+    <a class="hover:text-[#ED4245] duration-500 text-2xl text-white" href="/">> Home</a>
   {/if}
 
-  <div class="flex-none">
+  <div class="flex-none z-10">
     <div class="dropdown dropdown-hover dropdown-end drop-shadow-2xl">
-      <label tabindex="0" class="hover:text-[#ED4245] text-white text-2xl duration-500">[Menu]</label>
+      <!-- svelte-ignore a11y-label-has-associated-control -->
+      <label class="hover:text-[#ED4245] text-white text-2xl duration-500">[Menu]</label>
 
-      <ul tabindex="-1" class="dropdown-content menu p-2 bg-[#1C2125] w-48 rounded-none space-y-0.5">
-        {#each links as { name, url, isNewTab }}
+      <ul class="dropdown-content menu p-2 bg-[#1C2125] w-48 rounded-none space-y-0.5">
+        {#each links as { name, url }}
           <a
             class="hover:text-[#ED4245] hover:translate-x-2 duration-500 text-lg text-white"
-            target={isNewTab ? "_blank" : "_self"}
+            target={url.startsWith("/") ? "_self" : "_blank"}
             href={url}>{name}</a
           >
         {/each}
