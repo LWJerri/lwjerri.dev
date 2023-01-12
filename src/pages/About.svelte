dc39206f2a1f0e68d234f4a07b75be8b4ee5ba93
LWJerri <loginmanx@gmail.com>
2023-01-12 04:00:24 +0200

    Finish `About` page

--- src/pages/About.svelte
+++ src/pages/About.svelte
@@ -1,44 +1,170 @@
 <script lang="ts">
   import Navbar from "../components/Navbar.svelte";
   import Footer from "../components/Footer.svelte";
+  import { lazyLoad } from "../helpers/lazyLoad";
+  import { onMount } from "svelte";
+  import Image from "../../public/me.png";
 
-  const languages = [] as Array<string>;
-  const links = [] as Array<{ name: string; icon: string; url: string }>;
+  interface SocialLinks {
+    name: string;
+    icon: string;
+    url: string;
+  }
+
+  let languages = [] as Array<string>;
+  let technologies = [] as Array<string>;
+  let links = [] as Array<SocialLinks>;
+
+  onMount(async () => {
+    await dataLoader();
+
+    const anchor = window.location.hash.slice(1);
+
+    if (anchor) {
+      const findElement = document.getElementById(anchor);
+
+      findElement.getElementsByTagName("input")[0].checked = true;
+
+      if (findElement) {
+        window.scrollTo({
+          top: findElement.offsetTop,
+          behavior: "smooth",
+        });
+      }
+    }
+  });
+
+  async function dataLoader() {
+    const gistRequest = await fetch("https://api.lwjerri.dev/data");
+    const response = await gistRequest.json();
+
+    links = response.socialLinks;
+    technologies = response.techologies;
+    languages = response.languages;
+  }
+
+  function handleAnchorClick(event: MouseEvent & { currentTarget: EventTarget & HTMLAnchorElement }, id: string) {
+    event.preventDefault();
+
+    const link = event.currentTarget;
+    const anchor = new URL(link.href).hash;
+
+    const findElement = document.getElementById(id);
+
+    const {
+      location: { origin, pathname },
+    } = window;
+
+    navigator.clipboard.writeText(`${origin}${pathname}${anchor}`);
+
+    findElement.innerHTML = "[Copied]";
+
+    setTimeout(() => {
+      findElement.innerHTML = "[#]";
+    }, 500);
+  }
 </script>
 
-<div class="flex flex-col justify-between bg-[#0C0E10]" style="font-family: Monocraft;">
+<div class="flex flex-col justify-between bg-[#0C0E10] scroll-smooth" style="font-family: Monocraft;">
   <Navbar />
 
   <div class="flex flex-col space-y-16 max-w-4xl px-5 mx-auto">
     <div class="space-y-5">
-      <h1 class="text-[#ED4245] text-5xl font-bold outline-none">About</h1>
-      <img src="https://i.imgur.com/E6GUFW0.png" class="mx-auto" width="720" alt="X" />
-      <p class="text-white">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pulvinar, felis at pulvinar rhoncus, sem enim
-        dignissim justo, non interdum sapien dolor at ante. Aenean nisi ligula, consectetur a dignissim sit amet, porta
-        eget dui. Fusce quis vehicula turpis. Sed vel tellus velit. Fusce non nisl aliquet, posuere elit eu, faucibus
-        nibh.
-      </p>
+      <h1 class="text-[#ED4245] text-5xl font-bold outline-none text-center sm:flex sm:items-center sm:space-x-2">
+        <span>About</span>
+        <a
+          class="hover:text-[#ED4245] text-white duration-500 outline-none"
+          href="#about"
+          id="about"
+          on:click={(event) => handleAnchorClick(event, "about")}>[#]</a
+        >
+      </h1>
+
+      <img use:lazyLoad={Image} class="mx-auto" width="720" alt="ME" />
+
+      <div>
+        <a
+          class="hover:text-[#ED4245] text-white duration-500 outline-none"
+          href="#bio"
+          id="bio"
+          on:click={(event) => handleAnchorClick(event, "bio")}>[#]</a
+        >
+        <p class="text-white">
+          Hello, traveler! My name is Andrew, I was born in 2005 in Ukraine, I graduated from high school in 11th grade
+          and went to university in Kiev. I started my path to programming in the 5th grade, when I was playing servers
+          in the popular game Minecraft and I was very curious to know how it all worked under the hood. One winter, I
+          was walking with my friend Ilya and told him that I supposedly know how to write plugins, and he said it was
+          cool and would like to write them too, after coming home, I decided to learn how to do it in reality. That is
+          how out of sheer interest I started writing code and I love it so much! I chose backend development because I
+          am interested in knowing how the product works "behind the scenes", in a place where the user sees nothing and
+          does not know how the information is processed, how it is stored, etc.
+        </p>
+      </div>
+
+      <div>
+        <a
+          class="hover:text-[#ED4245] text-white duration-500 outline-none"
+          href="#story-about-nickname"
+          id="story-about-nickname"
+          on:click={(event) => handleAnchorClick(event, "story-about-nickname")}>[#]</a
+        >
+        <p class="text-white">
+          A little history about my nickname <span class="text-[#ED4245]">LWJerri</span>. It was in 2016, I had a weak
+          computer at the time and I always logged into the game Minecraft, but that day I decided to install the latest
+          version of the game, after which it crashed because of an error in the library LWGL. I sat for a long time,
+          looking at this error and for some reason decided to perpetuate it by taking the first two letters from that
+          library - <span class="text-[#ED4245]">LW</span>. This was not enough, the nickname came out too short and not
+          unique, so I remembered my favorite cartoon Tom and Jerry and my most favorite character from this cartoon -
+          little mouse Jerry. In those years my English was not very good and I made a mistake by writing
+          <span class="text-[#ED4245]">Jerri</span>
+          instead of Jerry to the prefix "LW". That's how the unique nickname
+          <span class="text-[#ED4245]">LWJerri</span> came about.
+        </p>
+      </div>
     </div>
 
     <div class="space-y-5">
-      <h1 class="text-[#ED4245] text-4xl">Technologies</h1>
+      <h1 class="text-[#ED4245] text-4xl text-center sm:flex sm:items-center sm:space-x-2">
+        <div class="break-words">
+          <span>What I use</span>
+        </div>
+        <a
+          class="hover:text-[#ED4245] text-white duration-500 outline-none"
+          href="#what-I-use"
+          id="what-I-use"
+          on:click={(event) => handleAnchorClick(event, "what-I-use")}>[#]</a
+        >
+      </h1>
       <p class="text-white">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pulvinar, felis at pulvinar rhoncus, sem enim
-        dignissim justo, non interdum sapien dolor at ante. Aenean nisi ligula, consectetur a dignissim sit amet, porta
-        eget dui.
+        When developing projects I use different technologies and programming languages, so that the product came out as
+        best as possible and satisfied my desires.
       </p>
 
+      <h2 class="text-[#ED4245] text-xl">Languages</h2>
       <p class="text-[#22B8CF]">{languages.join(", ")}</p>
+
+      <h2 class="text-[#ED4245] text-xl">Technologies</h2>
+      <p class="text-[#22B8CF]">{technologies.join(", ")}</p>
     </div>
 
     <div class="space-y-5">
-      <h1 class="text-[#ED4245] text-4xl">Socials</h1>
-      <p class="text-white">Leave a message on the form below or get in touch through Discord, Twitter or email.</p>
+      <h1 class="text-[#ED4245] text-4xl text-center sm:flex sm:items-center sm:space-x-2">
+        <span>Socials</span>
+        <a
+          class="hover:text-[#ED4245] text-white duration-500 outline-none"
+          href="#contact"
+          id="contact"
+          on:click={(event) => handleAnchorClick(event, "contact")}>[#]</a
+        >
+      </h1>
+      <p class="text-white">
+        If you have any questions for me or you just want to talk to me about something, you can use the social networks
+        below to contact me.
+      </p>
 
       <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 grid-flow-row gap-4">
         {#each links as { name, icon, url }}
-          <a href={url} class="hover:text-[#ED4245] duration-500"
+          <a href={url} target="_blank" rel="noreferrer" class="hover:text-[#ED4245] duration-500"
             ><div class="flex items-center justify-center text-lg space-x-2">
               {@html icon}<span>{name}</span>
             </div></a
@@ -50,3 +176,10 @@
 
   <Footer />
 </div>
+
+<style>
+  img {
+    opacity: 0;
+    transition: all 5s ease;
+  }
+</style>
