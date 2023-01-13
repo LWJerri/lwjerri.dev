99bccdc8b99bfa23a69c79656d42ff985f0c6983
LWJerri <loginmanx@gmail.com>
2023-01-13 04:14:55 +0200

    Move webVitals metrics to App

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,15 @@
   import Main from "./pages/Main.svelte";
   import Projects from "./pages/Projects.svelte";
   import About from "./pages/About.svelte";
+
+  import { webVitals } from "./helpers/vitals";
+
+  const analyticsId = import.meta.env.PUBLIC_VERCEL_ANALYTICS_ID;
+
+  webVitals({
+    path: window.location.pathname,
+    analyticsId,
+  });
 </script>
 
 <Router>
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,6 @@
 import App from "./App.svelte";
 import "./index.css";
 import { inject } from "@vercel/analytics";
-import { webVitals } from "./helpers/vitals";
-
-const analyticsId = import.meta.env.PUBLIC_VERCEL_ANALYTICS_ID;
-
-webVitals({
-  path: window.location.pathname,
-  analyticsId,
-});
 
 inject();
 
