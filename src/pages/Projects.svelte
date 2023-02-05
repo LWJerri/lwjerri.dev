f37a6fc2ee9859bc632be3856361e717e45ad719
LWJerri <loginmanx@gmail.com>
2023-02-05 10:58:21 +0200

    Use new blocks for projects

--- src/pages/Projects.svelte
+++ src/pages/Projects.svelte
@@ -81,9 +81,52 @@
     <div class="flex flex-col space-y-5 place-items-center">
       {#each projects as { name, description, stack, emoji, url, github }, id}
         {@const isHaveBigDescription = description.length > 45}
-        <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
-        <!-- svelte-ignore a11y-positive-tabindex -->
-        <div
+
+        <div class="space-y-4">
+          <details class="group rounded-lg bg-gray-50 p-6 [&_summary::-webkit-details-marker]:hidden">
+            <summary class="flex items-center justify-between cursor-pointer">
+              <h2 class="font-medium text-gray-900">Lorem ipsum dolor sit amet consectetur adipisicing?</h2>
+
+              <span class="relative ml-1.5 h-5 w-5 flex-shrink-0">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="absolute inset-0 opacity-100 group-open:opacity-0"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  stroke-width="2"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="absolute inset-0 opacity-0 group-open:opacity-100"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  stroke-width="2"
+                >
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
+              </span>
+            </summary>
+
+            <p class="mt-4 leading-relaxed text-gray-700">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic veritatis molestias culpa in, recusandae
+              laboriosam neque aliquid libero nesciunt voluptate dicta quo officiis explicabo consequuntur distinctio
+              corporis earum similique!
+            </p>
+          </details>
+        </div>
+
+        <!-- svelte-ignore a11y-no-noninteractive-tabindex
+         svelte-ignore a11y-positive-tabindex -->
+        <!-- <div
           id="project-{id}"
           tabindex="1"
           class="w-full md:w-11/12 lg:w-10/12 xl:w-1/2 collapse collapse-arrow bg-[#1D2123] rounded-none"
@@ -134,7 +177,7 @@
               >
             </div>
           </div>
-        </div>
+        </div>  -->
       {/each}
     </div>
   </div>
