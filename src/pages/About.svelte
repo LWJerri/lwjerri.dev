ff940ac3224a6d5696db69946c4f2df6fcdf38a0
LWJerri <loginmanx@gmail.com>
2023-02-13 18:57:41 +0200

    Add missed text color for some elements

--- src/pages/About.svelte
+++ src/pages/About.svelte
@@ -161,10 +161,12 @@
           href="#school-knowledge"
           id="school-knowledge"
           on:click={(event) => handleAnchorClick(event, "school-knowledge")}>[#]</a
-        > Went to school in 2011, where he completed the full 10th grade and the beginning of the 11th, was forced to change
-        places through the war, finishing school at another institution. In 2022 I went to the State University of Telecommunications
-        in Kyiv to study Software Engineering. While at school I took part in various IT competitions, where I did not take
-        a leading place but got a ton of new experience in the team and not only!
+        >
+        Went to school in 2011, where he completed the full 10th grade and the beginning of the 11th, was forced to change
+        places through the war, finishing school at another institution. In 2022 I went to the
+        <span class="text-[#22B8CF]">State University of Telecommunications</span> in Kyiv to study
+        <span class="text-[#22B8CF]">Software Engineering</span>. While at school I took part in various IT
+        competitions, where I did not take a leading place but got a ton of new experience in the team and not only!
       </p>
 
       <div class="divider text-[#5865F2] uppercase">Best works</div>
