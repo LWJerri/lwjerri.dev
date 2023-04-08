dad5eb2959841df63003a85c06c795e620c99113
LWJerri <loginmanx@gmail.com>
2023-04-08 19:32:18 +0300

    Add some optimal configs for Vercel deployment

--- svelte.config.js
+++ svelte.config.js
@@ -4,9 +4,13 @@ import { vitePreprocess } from "@sveltejs/kit/vite";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
-
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      envVarsInUse: ["VERCEL_TOKEN", "REDIS_USERNAME", "REDIS_PASSWORD", "REDIS_HOST", "REDIS_PORT"],
+      runtime: "nodejs18.x",
+      split: true,
+    }),
+    version: { name: String(Date.now()), pollInterval: 1000 * 10 },
   },
 };
 
