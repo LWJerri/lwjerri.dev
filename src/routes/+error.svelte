4da3b8b8c19a75930db9a098344187eeb18df95c
LWJerri <loginmanx@gmail.com>
2023-04-08 19:33:12 +0300

    Use images from `static` folder

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,14 +1,14 @@
 <svelte:head>
-  <title>Andrey Zontov - [Not found]</title>
+  <title>Andrey Zontov - Backstage?...</title>
 </svelte:head>
 
-<div class="p-1 sm:p-5 text-white flex flex-col flex-grow">
+<div class="p-1 sm:p-5 flex flex-col flex-grow">
   <div class="flex flex-col sm:flex-row items-center space-x-2 justify-center my-auto">
-    <h1 class="text-5xl font-bold uppercase my-auto text-center">Page not found</h1>
-    <img src="https://cdn.7tv.app/emote/614e96dd6251d7e000da7d22/3x.webp" alt="ROFL_IMAGE" />
+    <h1 class="text-5xl font-bold uppercase my-auto text-center">Backstage room</h1>
+    <img src="./cry.webp" alt="ROFL_IMAGE" />
   </div>
 
   <div class="w-full mt-auto">
-    <img class="ml-auto" src="https://cdn.7tv.app/emote/623f265a6f6be43db01ae06d/3x.webp" alt="ROFL_IMAGE" />
+    <img class="mr-auto" src="./peepo.webp" alt="ROFL_IMAGE" />
   </div>
 </div>
