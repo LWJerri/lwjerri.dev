0491c4debd971d4a1e2b2415ad6c92338a74e283
LWJerri <loginmanx@gmail.com>
2023-04-09 18:13:03 +0300

    Disable `prerender` for pages

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,7 +1,5 @@
 import type { PageLoad } from "./$types";
 
-export const prerender = true;
-
 const oneYearToMs = 1000 * 60 * 60 * 24 * 365;
 const calculateDate = new Date().getTime() - new Date("2005-04-28").getTime();
 const year = Math.floor(calculateDate / oneYearToMs);
