26f491831843fc729539c698e373e273dc6484f9
LWJerri <loginmanx@gmail.com>
2023-04-09 18:36:39 +0300

    Make `VERCEL_ANALYTICS_ID` available on client-side

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,7 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 
-export default defineConfig({ plugins: [sveltekit()] });
+export default defineConfig({
+  plugins: [sveltekit()],
+  define: { "import.meta.env.VERCEL_ANALYTICS_ID": JSON.stringify(process.env.VERCEL_ANALYTICS_ID) },
+});
