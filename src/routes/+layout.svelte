0ae32fa3d64d757795e9682d68407da2f5865a25
LWJerri <loginmanx@gmail.com>
2023-04-09 19:57:32 +0300

    upd

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,31 +1,31 @@
 <script lang="ts">
-  // import { browser, dev } from "$app/environment";
-  // import { beforeNavigate } from "$app/navigation";
-  // import { page, updated } from "$app/stores";
-  // import { webVitals } from "$lib/vitals";
-  // import { inject } from "@vercel/analytics";
+  import { browser, dev } from "$app/environment";
+  import { beforeNavigate } from "$app/navigation";
+  import { page, updated } from "$app/stores";
+  import { webVitals } from "$lib/vitals";
+  import { inject } from "@vercel/analytics";
   import "../app.css";
   import Footer from "../components/Footer.svelte";
   import Navbar from "../components/Navbar.svelte";
   import type { LayoutData } from "./$types";
 
-  // inject({ mode: dev ? "development" : "production" });
+  inject({ mode: dev ? "development" : "production" });
 
-  // beforeNavigate(({ willUnload, to }) => {
-  //   if ($updated && !willUnload && to?.url) {
-  //     location.href = to.url.href;
-  //   }
-  // });
+  beforeNavigate(({ willUnload, to }) => {
+    if ($updated && !willUnload && to?.url) {
+      location.href = to.url.href;
+    }
+  });
 
-  // let analyticsId = import.meta.env.VERCEL_ANALYTICS_ID;
+  let analyticsId = import.meta.env.VERCEL_ANALYTICS_ID;
 
-  // $: if (browser && analyticsId) {
-  //   webVitals({
-  //     path: $page.url.pathname,
-  //     params: $page.params,
-  //     analyticsId,
-  //   });
-  // }
+  $: if (browser && analyticsId) {
+    webVitals({
+      path: $page.url.pathname,
+      params: $page.params,
+      analyticsId,
+    });
+  }
 
   export let data: LayoutData;
 </script>
