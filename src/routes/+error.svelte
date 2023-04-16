9e73ba310c8db263d0195e2f77cd6e2e0e26c302
LWJerri <loginmanx@gmail.com>
2023-04-16 18:49:45 +0300

    Display images on the error page in deep links

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -5,10 +5,10 @@
 <div class="p-1 sm:p-5 flex flex-col flex-grow">
   <div class="flex flex-col sm:flex-row items-center space-x-2 justify-center my-auto">
     <h1 class="text-5xl font-bold uppercase my-auto text-center">Backstage room</h1>
-    <img src="./cry.webp" alt="ROFL_IMAGE" />
+    <img src="/cry.webp" alt="ROFL_IMAGE" />
   </div>
 
   <div class="w-full mt-auto">
-    <img class="mr-auto" src="./peepo.webp" alt="ROFL_IMAGE" />
+    <img class="mr-auto" src="/peepo.webp" alt="ROFL_IMAGE" />
   </div>
 </div>
