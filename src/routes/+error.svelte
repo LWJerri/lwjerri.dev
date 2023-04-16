6fdbfae7ff7c810412fc41c804af850227a127d7
LWJerri <loginmanx@gmail.com>
2023-04-16 23:32:23 +0300

    Set `select-none` for `+error.svelte` page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,7 +2,7 @@
   <title>Andrey Zontov - Backstage?...</title>
 </svelte:head>
 
-<div class="p-1 sm:p-5 flex flex-col flex-grow">
+<div class="p-1 sm:p-5 flex flex-col flex-grow select-none">
   <div class="flex flex-col sm:flex-row items-center space-x-2 justify-center my-auto">
     <h1 class="text-5xl font-bold uppercase my-auto text-center">Backstage room</h1>
     <img src="/cry.webp" alt="ROFL_IMAGE" />
