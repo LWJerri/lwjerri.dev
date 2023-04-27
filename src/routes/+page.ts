8f73857d067a1d5ee9dc0ced025516866510c61f
LWJerri <loginmanx@gmail.com>
2023-04-27 23:53:41 +0300

    Display the most accurate number

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -12,7 +12,8 @@ export const prerender = "auto";
 
 const today = dayjs();
 const birthdateObj = dayjs(new Date("2005-04-28").getTime()).utc().tz();
-const myAge = today.diff(birthdateObj, "year", true).toFixed(3);
+const myAge = Math.trunc(today.diff(birthdateObj, "year", true) * Math.pow(10, 3)) / Math.pow(10, 3);
+// Code from `myAge` constant: https://stackoverflow.com/a/48100007
 
 const emojis = [
   "(゜o゜;",
