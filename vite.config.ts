5842c189291252e0001f2adb17a2f5f254e4ee09
LWJerri <loginmanx@gmail.com>
2023-07-11 19:55:15 +0300

    Use correct `robots.txt` config, remove/update npm packages, remove `JSON.stringify()` from `vite.config.ts`

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [sveltekit()],
-  define: { "import.meta.env.VERCEL_ANALYTICS_ID": JSON.stringify(process.env.VERCEL_ANALYTICS_ID) },
+  define: { "import.meta.env.VERCEL_ANALYTICS_ID": process.env.VERCEL_ANALYTICS_ID },
 });
