b57946a3c30d1b1b64948c1b4369fd9737e2950b
LWJerri <loginmanx@gmail.com>
2023-07-13 11:56:02 +0300

    Update, optimize and rewrite my `/about` page

--- src/routes/about/+page.ts
+++ src/routes/about/+page.ts
@@ -1,20 +1,35 @@
 import type { PageLoad } from "./$types";
 
-const socialLinks = [
+const socialLinks: Array<{ name: string; icon: string; url: string }> = [
   {
     name: "Telegram",
-    icon: "<svg width='45' height='45' viewBox='0 0 45 45' fill='none' xmlns='http://www.w3.org/2000/svg'><path d='M39.7462 4.56187C39.0847 4.54353 38.4281 4.68165 37.83 4.965L21.6881 11.2087C17.8106 12.7087 13.9387 14.205 10.9556 15.3525C9.17755 16.0404 7.39692 16.7217 5.61373 17.3962C4.82623 17.6719 3.75748 18.0187 2.85186 19.0856C1.48686 20.9006 3.21373 22.4569 4.57498 23.3719C7.59373 24.3394 10.7156 25.2637 13.3012 26.1319C14.2556 29.4937 15.1706 32.8669 16.0762 36.2344C16.3762 36.9094 17.025 37.1606 17.6962 37.1681L17.6925 37.2019C17.6925 37.2019 18.2194 37.2544 18.7331 37.1306C19.3899 36.9634 19.9925 36.6296 20.4825 36.1612C21.1294 35.5537 22.8825 33.8287 23.8781 32.8537L31.3762 38.3888L31.4362 38.4225C31.4362 38.4225 32.265 39.0056 33.48 39.0881C34.0875 39.1294 34.8862 39.0131 35.5725 38.5106C36.2662 38.0044 36.7219 37.1944 36.9375 36.2681C37.5787 33.4706 41.8312 13.2337 42.5569 9.88312L42.5381 9.96187C43.0444 8.07562 42.8569 6.34687 41.685 5.32312C41.1461 4.85278 40.4612 4.58318 39.7462 4.56V4.56187Z' stroke='white' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'/></svg>",
+    icon: "<svg xmlns='http://www.w3.org/2000/svg' class='icon icon-tabler icon-tabler-brand-telegram' width='40' height='40' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor' fill='none' stroke-linecap='round' stroke-linejoin='round'><path stroke='none' d='M0 0h24v24H0z' fill='none'></path><path d='M15 10l-4 4l6 6l4 -16l-18 7l4 2l2 6l3 -4'></path></svg>",
     url: "https://t.me/LWJerri",
   },
   {
-    name: "Email",
-    icon: "<svg width='45' height='45' viewBox='0 0 52 45' fill='none' xmlns='http://www.w3.org/2000/svg'><path d='M8.57136 8.57141H42.8571C45.2142 8.57141 47.1428 10.5 47.1428 12.8571V38.5714C47.1428 40.9286 45.2142 42.8571 42.8571 42.8571H8.57136C6.21422 42.8571 4.28564 40.9286 4.28564 38.5714V12.8571C4.28564 10.5 6.21422 8.57141 8.57136 8.57141Z' stroke='white' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'/><path d='M47.1428 12.8571L25.7142 27.8571L4.28564 12.8571' stroke='white' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'/></svg>",
-    url: "mailto:hello@lwjerri.dev",
+    name: "Discord",
+    icon: "<svg xmlns='http://www.w3.org/2000/svg' class='icon icon-tabler icon-tabler-brand-discord' width='32' height='32' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor' fill='none' stroke-linecap='round' stroke-linejoin='round'><path stroke='none' d='M0 0h24v24H0z' fill='none'></path><path d='M8 12a1 1 0 1 0 2 0a1 1 0 0 0 -2 0'></path><path d='M14 12a1 1 0 1 0 2 0a1 1 0 0 0 -2 0'></path><path d='M15.5 17c0 1 1.5 3 2 3c1.5 0 2.833 -1.667 3.5 -3c.667 -1.667 .5 -5.833 -1.5 -11.5c-1.457 -1.015 -3 -1.34 -4.5 -1.5l-.972 1.923a11.913 11.913 0 0 0 -4.053 0l-.975 -1.923c-1.5 .16 -3.043 .485 -4.5 1.5c-2 5.667 -2.167 9.833 -1.5 11.5c.667 1.333 2 3 3.5 3c.5 0 2 -2 2 -3'></path><path d='M7 16.5c3.5 1 6.5 1 10 0'></path></svg>",
+    url: "https://discordlookup.com/user/432085389948485633",
   },
   {
-    name: "GitHub",
-    icon: "<svg width='45' height='45' viewBox='0 0 45 45' fill='none' xmlns='http://www.w3.org/2000/svg'><path d='M16.875 35.625C7.5 38.4375 7.5 30.9375 3.75 30M30 41.25V33.9938C30.0703 33.0997 29.9495 32.2009 29.6457 31.3571C29.3418 30.5133 28.8617 29.7439 28.2375 29.1C34.125 28.4438 40.3125 26.2125 40.3125 15.975C40.312 13.3572 39.305 10.8398 37.5 8.94375C38.3547 6.65345 38.2943 4.12191 37.3312 1.87501C37.3312 1.87501 35.1188 1.21876 30 4.65001C25.7025 3.4853 21.1725 3.4853 16.875 4.65001C11.7562 1.21876 9.54375 1.87501 9.54375 1.87501C8.58071 4.12191 8.52027 6.65345 9.375 8.94375C7.55649 10.8538 6.54848 13.394 6.5625 16.0313C6.5625 26.1938 12.75 28.425 18.6375 29.1563C18.0206 29.7937 17.5449 30.5539 17.2412 31.3874C16.9375 32.2208 16.8128 33.1089 16.875 33.9938V41.25' stroke='white' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'/></svg>",
-    url: "https://github.com/LWJerri",
+    name: "Twitch",
+    icon: "<svg xmlns='http://www.w3.org/2000/svg' class='icon icon-tabler icon-tabler-brand-twitch' width='32' height='32' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor' fill='none' stroke-linecap='round' stroke-linejoin='round'><path stroke='none' d='M0 0h24v24H0z' fill='none'></path><path d='M4 5v11a1 1 0 0 0 1 1h2v4l4 -4h5.584c.266 0 .52 -.105 .707 -.293l2.415 -2.414c.187 -.188 .293 -.442 .293 -.708v-8.585a1 1 0 0 0 -1 -1h-14a1 1 0 0 0 -1 1z'></path><path d='M16 8l0 4'></path><path d='M12 8l0 4'></path></svg>",
+    url: "https://www.twitch.tv/LWGerry",
+  },
+  {
+    name: "Twitter",
+    icon: "<svg xmlns='http://www.w3.org/2000/svg' class='icon icon-tabler icon-tabler-brand-twitter' width='32' height='32' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor' fill='none' stroke-linecap='round' stroke-linejoin='round'><path stroke='none' d='M0 0h24v24H0z' fill='none'></path><path d='M22 4.01c-1 .49 -1.98 .689 -3 .99c-1.121 -1.265 -2.783 -1.335 -4.38 -.737s-2.643 2.06 -2.62 3.737v1c-3.245 .083 -6.135 -1.395 -8 -4c0 0 -4.182 7.433 4 11c-1.872 1.247 -3.739 2.088 -6 2c3.308 1.803 6.913 2.423 10.034 1.517c3.58 -1.04 6.522 -3.723 7.651 -7.742a13.84 13.84 0 0 0 .497 -3.753c0 -.249 1.51 -2.772 1.818 -4.013z'></path></svg>",
+    url: "https://twitter.com/LWJerri",
+  },
+  {
+    name: "LinkedIn",
+    icon: "<svg xmlns='http://www.w3.org/2000/svg' class='icon icon-tabler icon-tabler-brand-linkedin' width='32' height='32' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor' fill='none' stroke-linecap='round' stroke-linejoin='round'><path stroke='none' d='M0 0h24v24H0z' fill='none'></path><path d='M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z'></path><path d='M8 11l0 5'></path><path d='M8 8l0 .01'></path><path d='M12 16l0 -5'></path><path d='M16 16v-3a2 2 0 0 0 -4 0'></path></svg>",
+    url: "https://www.linkedin.com/in/LWJerri",
+  },
+  {
+    name: "Email",
+    icon: "<svg xmlns='http://www.w3.org/2000/svg' class='icon icon-tabler icon-tabler-mail' width='40' height='40' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor' fill='none' stroke-linecap='round' stroke-linejoin='round'><path stroke='none' d='M0 0h24v24H0z' fill='none'></path><path d='M3 7a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-10z'></path><path d='M3 7l9 6l9 -6'></path></svg>",
+    url: "mailto:hello@lwjerri.dev",
   },
 ];
 
