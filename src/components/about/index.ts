c3f78a988f8752ba7084ffed41ce1277db0f243b
LWJerri <loginmanx@gmail.com>
2023-08-15 15:58:08 +0300

    Delete unused code

--- src/components/about/index.ts
+++
@@ -1,8 +0,0 @@
-export { default as AboutMe } from "./AboutMe.svelte";
-export { default as AboutMeHeader } from "./AboutMeHeader.svelte";
-export { default as AboutMyNickname } from "./AboutMyNickname.svelte";
-export { default as BackgroundEducation } from "./BackgroundEducation.svelte";
-export { default as BestWorks } from "./BestWorks.svelte";
-export { default as HobbiesAndInterests } from "./HobbiesAndInterests.svelte";
-export { default as Socials } from "./Socials.svelte";
-export { default as WhatIUse } from "./WhatIUse.svelte";
