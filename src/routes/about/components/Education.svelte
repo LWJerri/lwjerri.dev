dc001f7225a475110da053ef65edaf7757504c84
LWJerri <loginmanx@gmail.com>
2023-09-04 21:31:06 +0300

    Change university name

--- src/routes/about/components/Education.svelte
+++ src/routes/about/components/Education.svelte
@@ -11,7 +11,7 @@
   >
   Went to school in 2011, where he completed the full 10th grade and the beginning of the 11th, was forced to change places
   through the war, finishing school at another institution. In 2022 I went to the
-  <span class="text-[#22B8CF]">State University of Telecommunications</span> in Kyiv to study
+  <span class="text-[#22B8CF]">State University of information and communication technologies</span> in Kyiv to study
   <span class="text-[#22B8CF]">Software Engineering</span>. While at school I took part in various IT competitions,
   where I did not take a leading place but got a ton of new experience in the team and not only!
 </p>
