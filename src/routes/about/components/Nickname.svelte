a014b2b3dbeb465bb0fe416ddee2263583b04c3c
LWJerri <loginmanx@gmail.com>
2023-09-24 22:02:57 +0300

    Update story about my nickname

--- src/routes/about/components/Nickname.svelte
+++ src/routes/about/components/Nickname.svelte
@@ -11,11 +11,11 @@
   >
   A little history about my nickname <span class="text-[#22B8CF]">LWJerri</span>. It was in 2016, I had a weak computer
   at the time and I always logged into the game Minecraft, but that day I decided to install the latest version of the
-  game, after which it crashed because of an error in the library LWGL. I sat for a long time, looking at this error and
-  for some reason decided to perpetuate it by taking the first two letters from that library -
-  <span class="text-[#22B8CF]">LW</span>. This was not enough, the nickname came out too short and not unique, so I
-  remembered my favorite cartoon Tom and Jerry and my most favorite character from this cartoon - little mouse Jerry. In
-  those years my English was not very good and I made a mistake by writing
-  <span class="text-[#22B8CF]">Jerri</span> instead of Jerry to the prefix "LW". That's how the unique nickname LWJerri came
-  about.
+  game, after which it crashed because of an error in the library LWJGL. I sat for a long time, looking at this error
+  and for some reason decided to perpetuate it by taking the first three letters from that library -
+  <span class="text-[#22B8CF]">LWJ</span>. This was not enough, the nickname came out too short and not unique, so I
+  remembered my favorite cartoon Tom and Jerry and my most favorite character from this cartoon - little mouse Jerry 🥰.
+  In those years my English was not very good and I made a mistake by writing
+  <span class="text-[#22B8CF]">Jerri</span> instead of Jerry to the prefix "LWJ". That's how the unique nickname LWJerri
+  came about.
 </p>
