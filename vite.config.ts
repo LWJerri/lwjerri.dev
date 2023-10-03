4196984e240ddd2e3b323e77c471c3dec6b5fa7a
LWJerri <loginmanx@gmail.com>
2023-10-03 10:19:27 +0300

    Add `optimizeDeps`

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,17 @@ import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  optimizeDeps: {
+    include: [
+      "svelte-confetti",
+      "@tabler/icons-svelte",
+      "dayjs",
+      "dayjs/plugin/timezone",
+      "dayjs/plugin/utc",
+      "svelte/internal/disclose-version",
+      "web-vitals",
+    ],
+  },
   plugins: [sveltekit()],
   define: { "import.meta.env.VERCEL_ANALYTICS_ID": JSON.stringify(process.env.VERCEL_ANALYTICS_ID) },
 });
