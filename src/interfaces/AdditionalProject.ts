bd966373f0a882fd7a0655264bb6ad55ad620a11
LWJerri <loginmanx@gmail.com>
2023-10-03 19:02:20 +0300

    Add new interface `AdditionalProject`

---
+++ src/interfaces/AdditionalProject.ts
@@ -0,0 +1,5 @@
+export interface AdditionalProject {
+  name: string;
+  description: string;
+  url: string;
+}
--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,3 +1,4 @@
+export * from "./AdditionalProject";
 export * from "./Link";
 export * from "./LynardAPI";
 export * from "./NavigatorWithConnection";
