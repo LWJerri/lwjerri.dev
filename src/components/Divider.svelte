a1d9d41a6ae6ba796c751ba39f83ecbdd102dec1
LWJerri <loginmanx@gmail.com>
2024-06-09 03:17:58 +0200

    Add `Divider` component

---
+++ src/components/Divider.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  export let text: string;
+</script>
+
+<div class="flex items-center">
+  <span class="h-px flex-1 bg-white"></span>
+  <p class="shrink-0 px-6">{text}</p>
+  <span class="h-px flex-1 bg-white"></span>
+</div>
