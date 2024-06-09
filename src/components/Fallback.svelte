f4061841b4c540a06fe18a40389badf0a4a70b03
LWJerri <loginmanx@gmail.com>
2024-06-09 03:18:04 +0200

    Add `Fallback` component

---
+++ src/components/Fallback.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  export let title: string;
+  export let details: string;
+</script>
+
+<div class="grid place-content-center px-4">
+  <div class="px-1 text-center sm:px-5">
+    <div class="flex justify-center">
+      <h1 class="text-5xl font-bold text-[#ED4245]">{title}</h1>
+    </div>
+
+    <p class="mt-4 text-2xl">{details}</p>
+  </div>
+</div>
