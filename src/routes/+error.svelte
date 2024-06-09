9d0d2475f11ac23aa9e6db8c045097faa69a5687
LWJerri <loginmanx@gmail.com>
2024-06-09 03:23:45 +0200

    Use `Fallback` component for error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,30 +1,10 @@
 <script lang="ts">
-  import { onMount } from "svelte";
-
-  let url: string;
-
-  onMount(() => {
-    url = `/error/${Math.floor(Math.random() * 18 + 1)}.webp`;
-  });
+  import SvelteSeo from "svelte-seo";
+  import Fallback from "../components/Fallback.svelte";
 
   const PAGE_TITLE = "Andrii Zontov - Backstage?...";
 </script>
 
-<svelte:head>
-  <title>{PAGE_TITLE}</title>
+<SvelteSeo title={PAGE_TITLE} openGraph={{ title: PAGE_TITLE, site_name: PAGE_TITLE }} />
 
-  <meta name="og:title" content={PAGE_TITLE} />
-  <meta name="og:site_name" content={PAGE_TITLE} />
-</svelte:head>
-
-<div class="grid place-content-center px-4">
-  <div class="text-center">
-    <div class="flex justify-center">
-      <h1 class="text-9xl font-black text-[#ED4245]">404</h1>
-
-      <img class="hidden sm:block" src={url} alt="" />
-    </div>
-
-    <p class="mt-4 text-xl">Requested page not found</p>
-  </div>
-</div>
+<Fallback title="Umm..." details="Requested page not found" />
