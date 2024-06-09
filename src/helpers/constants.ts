393cd71267dbf47dc0cc40565f1d2458fbefb7f7
LWJerri <loginmanx@gmail.com>
2024-06-09 03:16:33 +0200

    Create `contants` file

---
+++ src/helpers/constants.ts
@@ -0,0 +1,13 @@
+import type { NavbarDropdownLink } from "../interfaces";
+
+export const NOT_PLAYING_ANYTHING = "Not playing anything.";
+
+export const location = { name: "Ukraine", url: "https://www.google.com/maps/place/Ukraine" };
+export const statsURL = "https://umami.lwjerri.dev/share/jV8DPlSgY0nXu0GL/lwjerri.dev";
+
+export const navbarDropdownLinks: NavbarDropdownLink[] = [
+  { name: "My projects", url: "/projects" },
+  { name: "About me ", url: "/about" },
+  { name: "My GitHub", url: "https://github.com/LWJerri", external: true },
+  { name: "Support me", url: "https://send.monobank.ua/8webyivBtV", external: true },
+];
