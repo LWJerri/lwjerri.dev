f94e05cc5aa60d5d67a1d25727af30082403c7af
LWJerri <loginmanx@gmail.com>
2024-06-09 03:25:34 +0200

    Remove `define` field

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,4 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 
-export default defineConfig({
-  plugins: [sveltekit()],
-  define: { "import.meta.env.VERCEL_ANALYTICS_ID": JSON.stringify(process.env.VERCEL_ANALYTICS_ID) },
-});
+export default defineConfig({ plugins: [sveltekit()] });
