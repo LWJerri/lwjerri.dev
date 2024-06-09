a4bf6fa6852939716b5b744bfbca25de4d9ad53c
LWJerri <loginmanx@gmail.com>
2024-06-09 03:16:21 +0200

    Delete `lib` folder

--- src/lib/umamiMetrics.ts
+++
@@ -1,8 +0,0 @@
-import { UMAMI_SITE_ID } from "$env/static/private";
-
-export async function umamiMetrics(apiURL: string, query: URLSearchParams, token: string) {
-  const headers = { "Content-Type": "application/json", Authorization: `Bearer ${token}` };
-  const requestURL = `${apiURL}/api/websites/${UMAMI_SITE_ID}/metrics?${query}`;
-
-  return await fetch(requestURL, { headers });
-}
