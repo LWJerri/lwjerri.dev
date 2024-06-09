58398395b2b32fe12ff231235927f97ed49852bf
LWJerri <loginmanx@gmail.com>
2024-06-09 03:18:18 +0200

    Rewrite `Update` component

--- src/components/Update.svelte
+++ src/components/Update.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
-  import { IconAlertSquare, IconReload, IconX } from "@tabler/icons-svelte";
+  import IconAlertSquare from "./svg/IconAlertSquare.svelte";
+  import IconReload from "./svg/IconReload.svelte";
+  import IconX from "./svg/IconX.svelte";
 
   let isAlertVisible = true;
 </script>
@@ -7,14 +9,14 @@
 <div
   class="{isAlertVisible
     ? 'block'
-    : 'hidden'} fixed bottom-0 flex w-full max-w-4xl flex-row items-center justify-between bg-[#5865F2] px-4 py-2 sm:mb-10 sm:w-4/5 sm:rounded-md"
+    : 'hidden'} fixed bottom-0 flex w-full max-w-5xl flex-row items-center justify-between bg-[#5865F2] px-4 py-2 sm:mb-10 sm:w-4/5 sm:rounded-md"
 >
   <div class="flex items-center space-x-2">
     <span class="group rounded-md bg-black/10 p-1 transition duration-500 hover:-rotate-45">
-      <IconAlertSquare size={24} stroke={1.5} class="duration-500 group-hover:rotate-45 group-hover:scale-110" />
+      <IconAlertSquare className="duration-500 group-hover:rotate-45 group-hover:scale-110" />
     </span>
 
-    <p class="break-words text-base">Update ready!</p>
+    <p class="break-words">Update ready!</p>
   </div>
 
   <div class="flex items-center space-x-2">
@@ -22,11 +24,11 @@
       class="group rounded-md bg-black/10 p-1 transition hover:bg-black/20"
       on:click={() => window.location.reload()}
     >
-      <IconReload size={24} stroke={1.5} class="duration-500 group-hover:rotate-180" />
+      <IconReload className="duration-500 group-hover:rotate-180" />
     </button>
 
     <button class="rounded-md bg-black/10 p-1 transition hover:bg-black/20" on:click={() => (isAlertVisible = false)}>
-      <IconX size={24} stroke={1.5} />
+      <IconX />
     </button>
   </div>
 </div>
