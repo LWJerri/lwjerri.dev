4aa8c7e24ebe7ac876d320f1f8b27c90ab434f04
LWJerri <loginmanx@gmail.com>
2024-06-09 03:25:25 +0200

    Remove `envVarsInUse` field

--- svelte.config.js
+++ svelte.config.js
@@ -5,11 +5,7 @@ import { vitePreprocess } from "@sveltejs/kit/vite";
 const config = {
   preprocess: vitePreprocess(),
   kit: {
-    adapter: adapter({
-      envVarsInUse: ["UMAMI_USERNAME", "UMAMI_PASSWORD", "KV_REST_API_URL", "KV_REST_API_TOKEN", "VERCEL_ANALYTICS_ID"],
-      runtime: "nodejs18.x",
-      split: false,
-    }),
+    adapter: adapter({ runtime: "nodejs18.x", split: false }),
     version: { name: String(Date.now()), pollInterval: 1000 * 10 },
   },
 };
