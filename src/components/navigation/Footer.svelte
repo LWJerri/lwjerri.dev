846bd19ca458303a33d6252c5d187749dcceb4b8
LWJerri <loginmanx@gmail.com>
2024-06-09 03:13:06 +0200

    Switch to WS to retrieve presence data

--- src/components/navigation/Footer.svelte
+++ src/components/navigation/Footer.svelte
@@ -1,92 +1,80 @@
 <script lang="ts">
-  import { IconCompass, IconEye, IconPlaylist } from "@tabler/icons-svelte";
   import { onMount } from "svelte";
-  import type { LynardAPI } from "../../interfaces";
+  import { NOT_PLAYING_ANYTHING, location, statsURL } from "../../helpers/constants";
+  import { fmtText } from "../../helpers/fmtText";
+  import type { LynardWS } from "../../interfaces";
+  import IconPlanet from "../svg/IconPlanet.svelte";
+  import IconPlaylist from "../svg/IconPlaylist.svelte";
 
-  export let pageView: number;
+  export let views: number | string;
 
-  const NOT_PLAYING_ANYTHING = "Not playing anything.";
+  let songArtUrl: string | null;
+  let playingSong = NOT_PLAYING_ANYTHING;
 
-  let lynardResponse: LynardAPI;
-  let spotifySong: string;
-  let isSongPlaying = false;
+  onMount(() => {
+    const socket = new WebSocket("wss://api.lanyard.rest/socket");
 
-  const city = { name: "Ukraine", url: "https://www.google.com/maps/place/Ukraine" };
-  const umamiStatsURL = "https://umami.lwjerri.dev/share/jV8DPlSgY0nXu0GL/lwjerri.dev";
+    socket.addEventListener("open", () =>
+      socket.send(JSON.stringify({ op: 2, d: { subscribe_to_id: "432085389948485633" } })),
+    );
 
-  const fmtText = (text: string) => {
-    const short = text.slice(0, 10).trimEnd() + "*";
+    socket.addEventListener("message", (event) => {
+      const parsedEvent: LynardWS = JSON.parse(event.data.toString("utf-8"));
 
-    return text.length > 10 ? short : text;
-  };
+      const { d } = parsedEvent;
 
-  async function getLynardStats() {
-    try {
-      const request = await fetch("https://api.lanyard.rest/v1/users/432085389948485633");
-      lynardResponse = await request.json();
-    } catch {}
+      if (!d.spotify) {
+        playingSong = NOT_PLAYING_ANYTHING;
+        songArtUrl = null;
 
-    if (!lynardResponse.data.spotify || !lynardResponse?.success) {
-      spotifySong = NOT_PLAYING_ANYTHING;
-      isSongPlaying = false;
+        return;
+      }
 
-      return;
-    }
+      const { song, artist, track_id, album_art_url } = d.spotify;
 
-    isSongPlaying = true;
+      const spotifySongURL = `https://open.spotify.com/track/${track_id}`;
 
-    const { song, artist, track_id } = lynardResponse.data.spotify;
+      const tagBuilder = document.createElement("a");
 
-    const spotifySongURL = `https://open.spotify.com/track/${track_id}`;
+      tagBuilder.classList.add("overflow-hidden", "text-ellipsis", "break-all", "duration-500", "hover:text-[#ED4245]");
+      tagBuilder.href = spotifySongURL;
+      tagBuilder.target = "_blank";
+      tagBuilder.textContent = `${fmtText(song)} [${fmtText(artist)}]`;
 
-    const tagBuilder = document.createElement("a");
+      playingSong = tagBuilder.outerHTML;
+      songArtUrl = album_art_url;
+    });
 
-    tagBuilder.classList.add("overflow-hidden", "text-ellipsis", "break-all", "duration-500", "hover:text-[#ED4245]");
-    tagBuilder.href = spotifySongURL;
-    tagBuilder.target = "_blank";
-    tagBuilder.textContent = `${fmtText(song)} [${fmtText(artist)}]`;
-
-    spotifySong = tagBuilder.outerHTML;
-  }
-
-  onMount(async () => {
-    await getLynardStats();
-
-    setInterval(async () => await getLynardStats(), 1000 * 10);
+    setInterval(() => socket.send(JSON.stringify({ op: 3 })), 1000 * 30);
   });
 </script>
 
-<!-- svelte-ignore a11y-no-redundant-roles -->
-<footer class="select-none py-2" role="contentinfo">
+<footer class="select-none py-2 text-lg">
   <div class="grid w-full grid-cols-1 items-center gap-3 px-1 sm:px-5 md:grid-cols-3">
     <div class="flex items-center space-x-2 text-left">
       <div>
-        <IconCompass size={24} stroke={1.5} />
+        <IconPlanet />
       </div>
 
-      <a class="duration-500 hover:text-[#ED4245]" href={city.url} target="_blank">{city.name}</a>
+      <a class="duration-500 hover:text-[#ED4245]" href={location.url} target="_blank">{location.name}</a>
     </div>
 
     <div class="flex items-center justify-start space-x-2 md:justify-center">
-      <div>
-        <IconEye size={24} stroke={1.5} />
-      </div>
-
-      <a class="duration-500 hover:text-[#ED4245]" href={umamiStatsURL} target="_blank">{pageView ?? 0} views</a>
+      <a class="duration-500 hover:text-[#ED4245]" href={statsURL} target="_blank">{views} views</a>
     </div>
 
     <div class="flex items-center justify-start space-x-2 md:justify-end">
-      {#if isSongPlaying}
+      {#if songArtUrl}
         <div class="avatar w-8">
-          <img class="rounded" src={lynardResponse.data.spotify?.album_art_url} alt="TRACK_IMAGE" />
+          <img class="rounded" src={songArtUrl} alt="TRACK_IMAGE" />
         </div>
       {:else}
         <div>
-          <IconPlaylist size={24} stroke={1.5} />
+          <IconPlaylist />
         </div>
       {/if}
 
-      <span>{@html spotifySong ?? NOT_PLAYING_ANYTHING}</span>
+      <span>{@html playingSong}</span>
     </div>
   </div>
 </footer>
