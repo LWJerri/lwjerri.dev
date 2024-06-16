601d8dc9c7716129488a6c9305c6c14265d98618
LWJerri <loginmanx@gmail.com>
2024-06-16 13:25:15 +0200

    Create new project card

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -2,30 +2,18 @@
   import { onMount } from "svelte";
   import SvelteSeo from "svelte-seo";
   import Fallback from "../../components/Fallback.svelte";
-  import IconChevronDown from "../../components/svg/IconChevronDown.svelte";
+  import { anchorGo } from "../../helpers/anchorGo";
   import { handleAnchorProjects } from "../../helpers/handleAnchorProjects";
   import type { PageData } from "./$types";
 
   export let data: PageData;
 
+  const projects = data.projects.sort((project) => (project.isClosed ? 1 : -1));
+
   const PAGE_TITLE = "Andrii Zontov - My projects";
   const PAGE_DESCRIPTION = "This page contains information about my projects, their descriptions, technologies, etc.";
 
-  onMount(async () => {
-    const anchor = window.location.hash.slice(1);
-
-    if (anchor) {
-      const findElement = document.getElementById(anchor);
-
-      if (findElement) {
-        findElement.getElementsByTagName("details")[0].open = true;
-
-        if (findElement) {
-          window.scrollTo({ top: findElement.offsetTop });
-        }
-      }
-    }
-  });
+  onMount(async () => anchorGo());
 </script>
 
 <SvelteSeo
@@ -34,64 +22,54 @@
   openGraph={{ title: PAGE_TITLE, description: PAGE_DESCRIPTION }}
 />
 
-{#if !data.projects.length}
+{#if !projects.length}
   <Fallback title="So silent here..." details="More projects will be added soon." />
 {:else}
-  <div class="mx-auto max-w-5xl space-y-5 px-1 sm:px-5">
-    <div class="flex flex-col items-center space-y-5">
-      {#each data.projects as { name, description, stack, emoji, url, github, isClosed }, id}
-        {@const isLongDescription = description.length > 50}
-        {@const getShortDescription = isLongDescription ? description.slice(0, 50) + "..." : description}
-        {@const longDescription = isLongDescription ? "..." + description.slice(50) : ""}
+  <div class="mx-auto grid w-full max-w-4xl gap-4 px-1 sm:px-5">
+    {#each projects as project, id}
+      <div class="rounded-md bg-[#1D2123] p-4" id="project-{id}">
+        <div class="space-y-5">
+          <div class="flex items-center justify-between">
+            <h1 class="text-lg">{project.name}</h1>
 
-        <div id="project-{id}">
-          <details class="group w-full rounded-md bg-[#1D2123] p-2 [&_summary::-webkit-details-marker]:hidden">
-            <summary class="flex cursor-pointer items-center justify-center">
-              <div class="hidden select-none text-5xl sm:block">{isClosed || !emoji ? "📃" : emoji}</div>
+            {#if project.isClosed}
+              <p class="box-decorator bg-[#ED4245]">Closed</p>
+            {/if}
+          </div>
 
-              <div class="flex flex-col">
-                <div class="flex items-center justify-between">
-                  <span class="text-lg">{name}</span>
+          <p>{project.description}</p>
 
-                  <span class="whitespace-nowrap rounded-md px-2.5 py-0.5 uppercase {isClosed ? 'hidden' : 'block'}"
-                    >Closed</span
-                  >
-                </div>
+          <div class="flex flex-wrap gap-2">
+            {#each project.stack as technology}
+              <div class="box-decorator bg-[#22B8CF]">{technology}</div>
+            {/each}
+          </div>
 
-                <span class="text-[#3F4549]">
-                  {!description.length ? "Description will be added soon." : getShortDescription}
-                </span>
-              </div>
+          <div class="flex items-center justify-end gap-2">
+            {#if project.urls}
+              {#each project.urls as url, i}
+                {@const title = `[URL #${i + 1}]`}
 
-              <div class="mx-2">
-                <IconChevronDown className="flex-shrink-0 transition duration-500 group-open:-rotate-180" />
-              </div>
-            </summary>
+                <a class="duration-500 hover:text-[#ED4245]" target="_blank" rel="noreferrer" href={url}>{title}</a>
+              {/each}
+            {/if}
 
-            <p class="text-[#6e767c]">{longDescription}</p>
-
-            <div class="mt-6 flex text-[#22B8CF]">{stack.join(", ")}</div>
-
-            <div class="flex w-full select-none flex-row justify-end space-x-2 text-lg">
-              {#if url}
-                <a class="duration-500 hover:text-[#ED4245]" target="_blank" rel="noreferrer" href={url}>[URL]</a>
-              {/if}
-
-              {#if github}
-                <a class="duration-500 hover:text-[#ED4245]" target="_blank" rel="noreferrer" href={github}>[GitHub]</a>
-              {/if}
-
-              <a
-                class="outline-none duration-500 hover:text-[#ED4245]"
-                target="_self"
-                rel="noreferrer"
-                href="#project-{id}"
-                on:click={(event) => handleAnchorProjects(event)}>[Share]</a
+            {#if project.github}
+              <a class="duration-500 hover:text-[#ED4245]" target="_blank" rel="noreferrer" href={project.github}
+                >[GitHub]</a
               >
-            </div>
-          </details>
+            {/if}
+
+            <a
+              class="outline-none duration-500 hover:text-[#ED4245]"
+              target="_self"
+              rel="noreferrer"
+              href="#project-{id}"
+              on:click={(event) => handleAnchorProjects(event)}>[Share]</a
+            >
+          </div>
         </div>
-      {/each}
-    </div>
+      </div>
+    {/each}
   </div>
 {/if}
