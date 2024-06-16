ec50d2a093b45b13aa9315fbd074b5a20c074ac9
LWJerri <loginmanx@gmail.com>
2024-06-16 12:40:11 +0200

    Add `Social` component to reduce duplication code

---
+++ src/components/Social.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { load as aboutLoad } from "../routes/about/+page";
+
+  const { socials } = aboutLoad();
+
+  export let social: keyof typeof socials;
+</script>
+
+<!-- prettier-ignore -->
+<a href={socials[social].url} target="_blank" class="box-decorator bg-[#ED4245] capitalize">
+  <svelte:component this={socials[social].icon} /><span>{social}</span>
+</a>
