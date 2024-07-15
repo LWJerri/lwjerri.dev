c589e7905db40246546c1589a71f23f8611b072f
LWJerri <loginmanx@gmail.com>
2024-07-15 10:54:00 +0200

    Optimize code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,20 +16,18 @@
 />
 
 <div class="hero flex justify-center">
-  <div class="hero-content text-center">
-    <div class="px-1 text-center sm:px-5">
-      <div class="font-bold text-[#5865F2] outline-none">
-        <h1 class="text-5xl">{OWNER}</h1>
-      </div>
+  <div class="hero-content px-1 text-center sm:px-5">
+    <div class="font-bold text-[#5865F2] outline-none">
+      <h1 class="text-5xl">{OWNER}</h1>
+    </div>
 
-      <p class="mt-5 text-2xl">
-        I am a <span style="color: #ED4245;">{data.age}</span> old backend developer
-      </p>
+    <p class="mt-5 text-2xl">
+      I am a <span style="color: #ED4245;">{data.age}</span> old backend developer
+    </p>
 
-      <div class="mt-5 text-2xl">
-        <a class="duration-500 hover:text-[#ED4245]" href="/projects">[Projects]</a>
-        <a class="duration-500 hover:text-[#ED4245]" href="/about">[About]</a>
-      </div>
+    <div class="mt-5 text-2xl">
+      <a class="duration-500 hover:text-[#ED4245]" href="/projects">[Projects]</a>
+      <a class="duration-500 hover:text-[#ED4245]" href="/about">[About]</a>
     </div>
   </div>
 </div>
