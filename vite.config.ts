f2e6904224ea576ed3579a7a13d482f1095ad7d5
Andrii Zontov <loginmanx@gmail.com>
2025-09-06 18:23:43 +0200

    chore: Add build bandle analyze & Return compact output code

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,14 @@
 import { sveltekit } from "@sveltejs/kit/vite";
+import analyze from "rollup-plugin-analyzer";
 import { defineConfig } from "vite";
 
-export default defineConfig({ plugins: [sveltekit()] });
+export default defineConfig({
+  plugins: [sveltekit(), analyze({ summaryOnly: true })],
+  build: {
+    rollupOptions: {
+      output: {
+        compact: true,
+      },
+    },
+  },
+});
