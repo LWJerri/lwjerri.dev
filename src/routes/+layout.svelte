5313844d9d0937e4474251a8afd4f941ec496c2b
Andrii Zontov <loginmanx@gmail.com>
2025-09-06 18:03:51 +0200

    chore: Use best practice to dynamically build `script` element

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,12 +2,41 @@
   import { browser, dev } from "$app/environment";
   import { updated } from "$app/state";
   import { injectSpeedInsights } from "@vercel/speed-insights";
+  import { onMount } from "svelte";
   import "../app.css";
   import Footer from "../components/navigation/Footer.svelte";
   import Header from "../components/navigation/Header.svelte";
   import Confetti from "../components/ui/Confetti.svelte";
   import Update from "../components/ui/Update.svelte";
   import type { LayoutData } from "./$types";
+
+  onMount(() => {
+    let umamiScript: HTMLScriptElement;
+    let rybbitScript: HTMLScriptElement;
+
+    if (window.location.hostname.toLocaleLowerCase().startsWith("lwjerri")) {
+      umamiScript = document.createElement("script");
+      rybbitScript = document.createElement("script");
+
+      umamiScript.async = true;
+      umamiScript.defer = true;
+      umamiScript.src = "https://umami.lwjerri.dev/script.js";
+      umamiScript.setAttribute("data-website-id", "e107c898-1d93-45b4-b9e8-f8e488489b6b");
+
+      rybbitScript.async = true;
+      rybbitScript.defer = true;
+      rybbitScript.src = "https://rybbit.lwjerri.dev/api/script.js";
+      rybbitScript.setAttribute("data-site-id", "1");
+
+      document.head.appendChild(umamiScript);
+      document.head.appendChild(rybbitScript);
+    }
+
+    return () => {
+      umamiScript.remove();
+      rybbitScript.remove();
+    };
+  });
 
   $: if (browser && !dev) {
     injectSpeedInsights({ framework: "sveltekit" });
