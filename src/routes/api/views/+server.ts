8563cd0ffc1da380a2d304d01a770ebd5dff988c
Andrii Zontov <loginmanx@gmail.com>
2025-12-13 21:25:44 +0100

    fix: Use `path` instead of `url` to get views statistics

--- src/routes/api/views/+server.ts
+++ src/routes/api/views/+server.ts
@@ -5,7 +5,7 @@ const client = getClient({ userId: UMAMI_USER_ID, secret: UMAMI_SECRET, apiEndpo
 
 export async function GET() {
   const { ok, data } = await client.getWebsiteMetrics(UMAMI_SITE_ID, {
-    type: "url",
+    type: "path",
     startAt: new Date("2023-07-13").getTime(),
     endAt: new Date().getTime(),
   });
