96ebcf9d30a29331e8b79a35c49719fb49c9a869
Andrii Zontov <loginmanx@gmail.com>
2026-04-12 14:29:51 +0200

    chore: Remove outdated `build` config

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,4 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [sveltekit(), analyze({ summaryOnly: true })],
-  build: {
-    rollupOptions: {
-      output: {
-        compact: true,
-      },
-    },
-  },
 });
