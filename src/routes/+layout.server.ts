ed861b812d138d26c24a2e2e5cf44b3861e052ed
Andrii Zontov <loginmanx@gmail.com>
2026-05-24 01:24:18 +0200

    chore: Disable prerender for layout server

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,7 +1,5 @@
 import type { LayoutServerLoad } from "./$types";
 
-export const prerender = false;
-
 export const load = (({ url }) => ({
   isAnalyticsEnabled: url.hostname.startsWith("lwjerri"),
 })) satisfies LayoutServerLoad;
