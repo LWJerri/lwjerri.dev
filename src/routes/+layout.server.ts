df5cdb5553886e3cfd35449661e3e306a7b208da
Andrii Zontov <loginmanx@gmail.com>
2026-05-24 00:24:32 +0200

    chore: Revert latest changes

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,7 +1,5 @@
 import type { LayoutServerLoad } from "./$types";
 
-export const load = (({ url }) => {
-  return {
-    isAnalyticsEnabled: url.hostname.startsWith("lwjerri"),
-  };
-}) satisfies LayoutServerLoad;
+export const load = (({ url }) => ({
+  isAnalyticsEnabled: url.hostname.startsWith("lwjerri"),
+})) satisfies LayoutServerLoad;
