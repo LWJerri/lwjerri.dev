e67178283bb6bf643b6382ec59c039e4e354b57d
Andrii Zontov <loginmanx@gmail.com>
2026-05-23 21:39:40 +0200

    chore: Use time-based version name indicator

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,7 @@ const config = {
   preprocess: vitePreprocess(),
   kit: {
     adapter: adapter({ runtime: "nodejs24.x", split: false }),
-    version: { name: new Date().getTime(), pollInterval: 1000 * 10 },
+    version: { name: String(Date.now()), pollInterval: 1000 * 10 },
   },
 };
 
