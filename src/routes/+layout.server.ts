5f4ec8bb8cfaf6f539cf9981ca95fbc85855c0f1
Andrii Zontov <loginmanx@gmail.com>
2026-05-24 00:37:52 +0200

    chore: Log `url.hostname`

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,5 +1,9 @@
 import type { LayoutServerLoad } from "./$types";
 
-export const load = (({ url }) => ({
-  isAnalyticsEnabled: url.hostname.startsWith("lwjerri"),
-})) satisfies LayoutServerLoad;
+export const load = (({ url }) => {
+  console.error("url.hostname", url.hostname);
+
+  return {
+    isAnalyticsEnabled: url.hostname.startsWith("lwjerri"),
+  };
+}) satisfies LayoutServerLoad;
